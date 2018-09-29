a9ff2644e976930122f23c63fe7d3b4a3f8428ba
CTHobbit <ben.schenkman@gmail.com>
2018-09-29 12:44:06 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,18 +17,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices that the page title and header indicate a to-do list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
         
 
-# The first thing he sees is the option to enter a to-do item
+        # The first thing he sees is the option to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-# He types "Buy turkey feathers" into a text box (Bob collects feathers from molting birds)
+        # He types "Buy turkey feathers" into a text box (Bob collects feathers from molting birds)
+        inputbox.send_keys('Buy peacock feathers')  
 
-# When he hits enter, the page updates, and now the page lists
-# "1: Buy turkey feathers" as an item in a to-do list
+        # When he hits enter, the page updates, and now the page lists
+        # "1: Buy turkey feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)  
+        time.sleep(1)  
 
-# There is still a text box inviting him to add more items
-# He enters "use turkey feathers to make a fan" (Bob is very creative)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting him to add more items
+        # He enters "use turkey feathers to make a fan" (Bob is very creative)
+        self.fail('Finish the test!')
 
 # The page updates again, and now shows both items on the list
 
